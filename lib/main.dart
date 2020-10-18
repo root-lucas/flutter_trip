7a1a046d025bbcc15b4c7de60800ff4ab3397bfe
root-lucas <945832032@qq.com>
2020-10-18 13:09:36 +0800

    add: HomePage banner show

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
 import './navigator/tab_navigator.dart';
 
 void main() => runApp(MyApp());
