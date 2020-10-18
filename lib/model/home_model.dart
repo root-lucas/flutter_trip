9ad1d7c6a4be1b3f5b58054949690074769e166d
root-lucas <945832032@qq.com>
2020-10-18 23:11:41 +0800

    feat: HomePage sub-navigation

--- lib/model/home_model.dart
+++ lib/model/home_model.dart
@@ -3,7 +3,6 @@ import 'package:flutter_trip/model/config_model.dart';
 import 'package:flutter_trip/model/grid_nav_model.dart';
 import 'package:flutter_trip/model/sales_box_model.dart';
 
-// 主页模型
 class HomeModel {
   final ConfigModel config;
   final List<CommonModel> bannerList;
@@ -12,25 +11,24 @@ class HomeModel {
   final List<CommonModel> subNavList;
   final SalesBoxModel salesBox;
 
-  HomeModel({
-      this.config,
+  HomeModel(
+      {this.config,
       this.bannerList,
       this.localNavList,
       this.gridNav,
       this.subNavList,
-      this.salesBox
-  });
+      this.salesBox});
 
   factory HomeModel.fromJson(Map<String, dynamic> json) {
     var localNavListJson = json['localNavList'] as List;
     List<CommonModel> localNavList =
         localNavListJson.map((i) => CommonModel.fromJson(i)).toList();
 
-    var bannerListJson = json['localNavList'] as List;
+    var bannerListJson = json['bannerList'] as List;
     List<CommonModel> bannerList =
         bannerListJson.map((i) => CommonModel.fromJson(i)).toList();
 
-    var subNavListJson = json['localNavList'] as List;
+    var subNavListJson = json['subNavList'] as List;
     List<CommonModel> subNavList =
         subNavListJson.map((i) => CommonModel.fromJson(i)).toList();
 
